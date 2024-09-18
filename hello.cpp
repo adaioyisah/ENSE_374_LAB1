5de0e4e3012a942a4162ddcb09eef40deb135315
Adaiyisah <adai.yisah@gmail.com>
2024-09-18 08:08:03 -0600

    created hello world programme

---
+++ hello.cpp
@@ -0,0 +1,16 @@
+//c++ programe to display "Hello World"
+
+//input/output funtions header file
+#include <iostream>
+using namespace std; 
+
+//main function 
+
+int main () 
+{
+    //print "Hello World"
+    cout << "Hello World" 
+
+    return 0; 
+    
+}
